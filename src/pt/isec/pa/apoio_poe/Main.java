25a49e85f6e3c74189d5017f6e6f187da9303dd1
serac01 <serac01@gmail.com>
2022-04-02 17:23:09 +0100

    Teste para passar no github

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -2,6 +2,6 @@ package pt.isec.pa.apoio_poe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world, Github Test");
     }
 }
