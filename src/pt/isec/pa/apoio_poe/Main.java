595791cb8603aff2cf2b75d489b6066a192b48f7
serac01 <serac01@gmail.com>
2022-04-06 10:06:23 +0100

    Estrutura basica das fases

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -1,7 +1,12 @@
 package pt.isec.pa.apoio_poe;
 
+import pt.isec.pa.apoio_poe.model.fsm.PhaseContext;
+import pt.isec.pa.apoio_poe.ui.text.PhaseUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world, Github Test");
+        PhaseContext fsm = new PhaseContext();
+        PhaseUI ui = new PhaseUI(fsm);
+        ui.start();
     }
 }
---
+++ src/pt/isec/pa/apoio_poe/model/fsm/SecondPhaseState.java
@@ -0,0 +1,19 @@
+package pt.isec.pa.apoio_poe.model.fsm;
+
+import pt.isec.pa.apoio_poe.model.data.Phase;
+
+public class SecondPhaseState extends PhaseStateAdapter {
+    SecondPhaseState(PhaseContext context, Phase phase){
+        super(context,phase);
+        phase.setCurrentPhase(1);
+    }
+
+    @Override
+    public boolean previousPhase() {
+        changeState(new FirstPhaseState(context,phase));
+        return true;
+    }
+
+    @Override
+    public PhaseState getState() { return PhaseState.PHASE_2; }
+}
