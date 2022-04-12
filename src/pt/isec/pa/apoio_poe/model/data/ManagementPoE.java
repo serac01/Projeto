1e65d0f72b4ecb412503264aceaea65d07cd85e5
serac01 <serac01@gmail.com>
2022-04-12 14:32:42 +0100

    Estrutura incluindo ficheiros CSV

---
+++ src/pt/isec/pa/apoio_poe/model/data/ManagementPoE.java
@@ -0,0 +1,9 @@
+package pt.isec.pa.apoio_poe.model.data;
+
+import java.util.ArrayList;
+
+public class ManagementPoE {
+    private final ArrayList<Student> students = new ArrayList<>();
+    private final ArrayList<Teacher> teachers = new ArrayList<>();
+    private final ArrayList<Proposal> proposals = new ArrayList<>();
+}
