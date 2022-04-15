08ca9035838b5ee66db88c60308f4659e893dd69
RobertoTarta <102826147+RobertoTarta@users.noreply.github.com>
2022-04-15 22:20:22 +0100

    Update Proposal.java

--- src/pt/isec/pa/apoio_poe/model/data/Proposal.java
+++ src/pt/isec/pa/apoio_poe/model/data/Proposal.java
@@ -15,4 +15,12 @@ public class Proposal {
     public String getTitle() { return title; }
 
     public void setTitle(String title) { this.title = title; }
+    
+        @Override
+    public String toString() {
+        return "Proposal{" +
+                "identification=" + identification +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
