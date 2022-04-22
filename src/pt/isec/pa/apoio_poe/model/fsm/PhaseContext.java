07f20154128edf42f1aa23c04be593f566b2392d
serac01 <serac01@gmail.com>
2022-04-22 15:39:07 +0100

    Edição e Adição de alunos completa

--- src/pt/isec/pa/apoio_poe/model/fsm/PhaseContext.java
+++ src/pt/isec/pa/apoio_poe/model/fsm/PhaseContext.java
@@ -2,9 +2,10 @@ package pt.isec.pa.apoio_poe.model.fsm;
 
 import pt.isec.pa.apoio_poe.model.data.ManagementPoE;
 import pt.isec.pa.apoio_poe.model.data.Phase;
-import pt.isec.pa.apoio_poe.model.data.ManagementPoE;
+import pt.isec.pa.apoio_poe.model.data.Student;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PhaseContext {
     IPhaseState state;
@@ -28,11 +29,24 @@ public class PhaseContext {
     public boolean previousPhase(){ return state.previousPhase(); }
 
     //Get data
-    public void addStudents(String filename) throws IOException { management.addStudents(filename); }
+    public void addStudents(String filename) throws IOException {
+        ArrayList<Student> aux;
+        aux=FirstPhaseState.addStudents(filename,management.getStudent());
+        if(aux!=null)
+            management.setStudent(aux);
+    }
 
-    public void editStudent(long number, String toUpdate, int option) { management.editStudent(number,toUpdate,option); }
+    public String editStudent(long number, String toUpdate, int option) {
+        ArrayList<Student> aux;
+        aux=FirstPhaseState.editStudent(number,toUpdate,option,management.getStudent());
+        if (aux!=null)
+            management.setStudent(aux);
+        else
+            return "\nYou have entered wrong data, please confirm again\n";
+        return "";
+    }
 
-    public void showStudents(){ management.showStudents(); }
+    public void showStudents(){ FirstPhaseState.showStudents(management.getStudent()); }
 
     public int getCurrentPhase(){return phase.getCurrentPhase();}
 }
